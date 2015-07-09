019e3e1f001e1b4c4814b6f2647e844f9c30f288
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-07-09 09:06:14 -0500

    Pequeñas modificaciones y ordenando el directorio un poco mas

--- examples/macruby/list_chrome_all_tab_title.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/macruby
-
-framework "ScriptingBridge"
-
-chrome = SBApplication.applicationWithBundleIdentifier("com.google.Chrome")
-chrome.windows[0].tabs.each {|tab| puts tab.title}  
