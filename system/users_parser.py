cf2328d297907dd1c9a9b83f771d29b917963486
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-07-10 10:09:42 -0500

    Correcciones al script users_parser

--- system/users_parser.py
+++ system/users_parser.py
@@ -2,9 +2,7 @@
 Print all the users and their login shells
 """
 
-from __future__ import print_function
 import pwd
-
 
 # Get the users from /etc/passwd
 def getusers():
@@ -12,5 +10,5 @@ def getusers():
     for user in users:
         print('{0}:{1}'.format(user.pw_name, user.pw_shell))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     getusers()
